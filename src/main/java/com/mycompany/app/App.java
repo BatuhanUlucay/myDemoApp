a792bced34f74c9be52a062012de06d9969b1223
BatuhanUlucay <hulucay@etu.edu.tr>
2018-09-29 12:49:45 +0330

    Getting ready for deployment to heroku

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -1,5 +1,12 @@
 package com.mycompany.app;
 import java.util.*;
+import static spark.Spark.get;
+import static spark.Spark.port;
+import static spark.Spark.post;
+
+import spark.ModelAndView;
+import spark.template.mustache.MustacheTemplateEngine;
+
 /**
  * Hello world!
  *
@@ -12,8 +19,111 @@ public class App
 
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        port(getHerokuAssignedPort());
+
+        get("/", (req, res) -> "Hello, World");
+
+        post("/compute", (req, res) -> {
+          //System.out.println(req.queryParams("input1"));
+          //System.out.println(req.queryParams("input2"));
+
+          String input1 = req.queryParams("input1");
+          ArrayList<Integer> inputList1 = new ArrayList<>();
+          if(input1.equals("")){
+
+            inputList1.add(0);
+          }
+
+          else{
+            java.util.Scanner sc1 = new java.util.Scanner(input1);
+            sc1.useDelimiter("[;\r\n]+");
+            while (sc1.hasNext())
+            {
+              int value = Integer.parseInt(sc1.next().replaceAll("\\s",""));
+              inputList1.add(value);
+            }
+                sc1.close();
+          }
+          
+          //System.out.println(inputList1);
+
+          String input2 = req.queryParams("input2");
+          ArrayList<Integer> inputList2 = new ArrayList<>();
+          if(input2.equals("")){
+
+            inputList2.add(0);
+          }
+
+          else{
+            java.util.Scanner sc2 = new java.util.Scanner(input2);
+            sc2.useDelimiter("[;\r\n]+");
+            while (sc2.hasNext())
+            {
+              int value = Integer.parseInt(sc2.next().replaceAll("\\s",""));
+              inputList2.add(value);
+            }
+                sc2.close();
+          }
+
+          String input3 = req.queryParams("input3");
+          ArrayList<String> inputList3 = new ArrayList<>();
+          if(input3.equals("")){
+
+            inputList2.add(null);
+          }
+
+          else{
+            java.util.Scanner sc3 = new java.util.Scanner(input3);
+            sc3.useDelimiter("[;\r\n]+");
+            while (sc3.hasNext())
+            {
+              String value = (sc3.nextLine().replaceAll("\\s",""));
+              inputList3.add(value);
+            }
+                sc3.close();
+          }
+
+          String input4 = req.queryParams("input4").replaceAll("\\s","");
+          char inputChar;
+
+          if(input4.equals("")){
+                inputChar = ' ';
+
+          }
+
+          else{
+            java.util.Scanner sc4 = new Scanner(input4);
+            sc4.useDelimiter("[;\r\n]+");
+            inputChar = sc4.next().charAt(0);
+            sc4.close();
+          }
+
+          ArrayList<String> result = App.suggestPassword(inputList1, inputList2, inputList3, inputChar);
+
+         Map map = new HashMap();
+          map.put("result", result);
+          return new ModelAndView(map, "compute.mustache");
+        }, new MustacheTemplateEngine());
+
+
+        get("/compute",
+            (rq, rs) -> {
+              Map map = new HashMap();
+              map.put("result", "not computed yet!");
+              return new ModelAndView(map, "compute.mustache");
+            },
+            new MustacheTemplateEngine());
     }
+
+    static int getHerokuAssignedPort() {
+        ProcessBuilder processBuilder = new ProcessBuilder();
+        if (processBuilder.environment().get("PORT") != null) {
+            return Integer.parseInt(processBuilder.environment().get("PORT"));
+        }
+        return 4567; //return default port if heroku-port isn't set (i.e. on localhost)
+    }
+
+    
 
 
     public static ArrayList<String> suggestPassword(ArrayList<Integer> array1, ArrayList<Integer> array2, ArrayList<String> str, char a){
